5ec72a2a699b8ca99753a41a3b3cd671fd82385a
Aaron Saunders <aaron@clearlyinnovative.com>
2013-01-27 14:09:11 -0500

    set the model id on the objects to get the desired results

--- app/models/Stock.js
+++ app/models/Stock.js
@@ -1,38 +1,51 @@
 exports.definition = {
- 
-    config: {
-        "columns": {
-        	company: "String",
-        	symbol: "String",
-        	price: "number"
+
+    config : {
+        "columns" : {
+            company : "String",
+            symbol : "String",
+            price : "number"
         },
-        "defaults": {
+        "defaults" : {
         },
-        "adapter": {
-            "type": "stocksAdapter",
-            "collection_name": "Stocks"
+        "adapter" : {
+            "type" : "stocksAdapter",
+            "collection_name" : "Stocks"
         }
-    },      
- 
-    extendModel: function(Model) {      
+    },
+
+    extendModel : function(Model) {
         _.extend(Model.prototype, {
- 
+
+            initialize : function(attributes) {
+                this.set({
+                    "id" : attributes['idAttribute'] || guid()
+                });
+            },
             // extended functions go here
- 
-        }); // end extend
- 
+
+        });
+        // end extend
+
         return Model;
     },
- 
- 
-    extendCollection: function(Collection) {        
-        _.extend(Collection.prototype, {
- 
-            // extended functions go here           
 
-        }); // end extend
- 
+    extendCollection : function(Collection) {
+        _.extend(Collection.prototype, {
+
+            // extended functions go here
+
+        });
+        // end extend
+
         return Collection;
     }
- 
+}
+
+function S4() {
+    return ((1 + Math.random()) * 65536 | 0).toString(16).substring(1);
+}
+
+function guid() {
+    return S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4();
 }
